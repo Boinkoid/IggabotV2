1aca908382251cc9b7d408286402d9976d466a81
Delta <Delta@DESKTOP-F05EF1P>
2026-05-11 05:00:15 -0700

    Commit

--- Iggabot/src/com/iggacorp/Starter.java
+++ Iggabot/src/com/iggacorp/Starter.java
@@ -3,6 +3,11 @@ package com.iggacorp;
 import com.iggacorp.logic.Main;
 import com.iggacorp.logic.GUI.GUI;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+
 public class Starter {
 
 	public static void main(String[] args) throws Exception {
